cc7e873f93c53e74efdbc65f713a88fadb8ef999
Oumaymakhl <oumaymakahouli19@gmail.com>
2024-02-29 11:10:58 +0100

    apimodif

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SadminController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\ReunionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,7 +29,7 @@ Route::post('/admin/login', [AdminController::class, 'login']);
 Route::post('/user/signup', [Controller::class, 'signup']);
 Route::post('/user/login', [Controller::class, 'login']);
 Route::get('/companies/index', [CompanyController::class, 'index']); 
- Route::get('/companies/show/{id}', [CompanyController::class, 'show']); 
+Route::get('/companies/show/{id}', [CompanyController::class, 'show']); 
 Route::put('/companies/update/{id}', [CompanyController::class, 'update']); 
 Route::delete('/companies/destroy/{id}', [CompanyController::class, 'destroy']); 
 
@@ -35,3 +37,8 @@ Route::get('/user', [Controller::class, 'index']); // Liste des utilisateurs
 Route::get('/user/{id}', [Controller::class, 'show']); // Afficher un utilisateur spécifique
 Route::put('/user/{id}', [Controller::class, 'update']); // Mettre à jour les informations d'un utilisateur
 Route::delete('/user/{id}', [Controller::class, 'destroy']); // Supprimer un utilisateur
+Route::delete('/user/{id}', [Controller::class, 'destroy']); // Supprimer un utilisateur
+Route::post('/reunions', [ReunionController::class, 'create_reunion']); // Create
+Route::get('/reunions', [ReunionController::class, 'index']); // Read
+Route::put('/reunions/{id}', [ReunionController::class, 'update']); // Update
+Route::delete('/reunions/{id}', [ReunionController::class, 'destroy']); // Delete
