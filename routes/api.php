b9377a9f9d02295bf7c8e0ace34d5bd6fb091808
Oumaymakhl <oumaymakahouli19@gmail.com>
2024-04-26 11:05:19 +0200

    ap

--- routes/api.php
+++ routes/api.php
@@ -97,7 +97,6 @@ Route::get('/admin/{id}', [AdminController::class, 'show']);
 Route::get('/admin/{id}/edit', [AdminController::class, 'edit']);
 Route::put('/admin/{id}', [AdminController::class, 'update']);
 Route::delete('/admin/{id}', [AdminController::class, 'destroy']);
-Route::delete('/admin/{id}', [AdminController::class, 'destroy']);
 Route::post('user/logout', [Controller::class, 'userLogout']);
 Route::post('admin/logout', [AdminController::class, 'adminLogout']);
 Route::put('/tasks/{id}/calculate-time-spent', [TaskController::class, 'calculateTimeSpent']);
