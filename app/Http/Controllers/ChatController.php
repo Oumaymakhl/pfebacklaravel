5a9ec98a0d20749605094f27409e04751d2d1d24
Oumaymakhl <oumaymakahouli19@gmail.com>
2024-05-29 01:15:09 +0200

    chat en groupe

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -1,34 +1,59 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Tymon\JWTAuth\Facades\JWTAuth;
 use Illuminate\Http\Request;
 use App\Models\Chat;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use App\Events\ChatSent;
+use App\Models\admin;
+use Pusher\Pusher;
 
 class ChatController extends Controller
 {
     public function sendMessage(Request $request)
     {
+    $token = JWTAuth::getToken();
+    $payload = JWTAuth::getPayload($token)->toArray();
+    $group=$payload['group'];
+    $id = $payload['sub'];
+    $type=$payload['type'];
+    if($type=="user"){
+      $user=  User::find($id);
+    }else{
+        $user=  admin::find($id);
+    }
         $chat = Chat::create([
-            'sender_id' => Auth::id(),
-            'receiver_id' => $request->receiver_id,
+            'sender_id' => $id,
+            'receiver_id' => $group,
             'message' => $request->message,
+            'name'=>$user->nom,
+            'photo_url'=>$user->profile_photo,
+            'email'=>$user->email,
         ]);
-        event(new ChatSent($request->message, $request->receiver_id));
-
+        $pusher = new Pusher(env("PUSHER_APP_KEY"),env("PUSHER_APP_SECRET"),env("PUSHER_APP_ID"),[
+            'cluster' => 'mt1',
+            'useTLS' => true,
+                    ],);
+        $pusher->trigger('groupChanel'.$group, 'my-event', $chat);
         return response()->json($chat);
     }
 
-    public function getMessages($senderId, $receiverId)
+    public function getMessages(Request $request)
     {
-        $messages = Chat::where(function ($query) use ($senderId, $receiverId) {
-            $query->where('sender_id', $senderId)->where('receiver_id', $receiverId);
-        })->orWhere(function ($query) use ($senderId, $receiverId) {
-            $query->where('sender_id', $receiverId)->where('receiver_id', $senderId);
-        })->orderBy('created_at', 'asc')->get();
-
-        return response()->json($messages);
+        $token = JWTAuth::getToken();
+        $payload = JWTAuth::getPayload($token)->toArray();
+        $group=$payload['group'];
+        $id = $payload['sub'];
+        $type = $payload['type'];
+        if($type=="user"){
+            $email=  User::find($id)->email;
+          }else{
+              $email= admin::find($id)->email;
+          }
+        $messages = Chat::where('receiver_id','=',$group)->get();
+       
+        return response()->json(['message'=>$messages,'my'=>$id,"group"=>$group,"email"=>$email],200);
     }
 }
