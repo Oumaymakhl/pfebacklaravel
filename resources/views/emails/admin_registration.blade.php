448a390ada3909ef35008b22ac4459f52cd2ec84
Oumaymakhl <oumaymakahouli19@gmail.com>
2024-06-06 15:07:51 +0200

    emails

--- resources/views/emails/admin_registration.blade.php
+++ resources/views/emails/admin_registration.blade.php
@@ -1,7 +1,13 @@
-<p>Hello{{ $admin->prenom }},</p>
+<div style="border: 1px solid #ccc; padding: 10px; width: 300px;">
+    <div style="background-color: #004085; color: #fff; padding: 10px;">
+        <h2 style="color: white;">Admin Registration</h2>
+    </div>
+    <div style="padding: 10px;">
+    <p>Hello{{ $admin->prenom }},</p>
 <p>Your administrator account has been successfully created. Here are your login details:</p>
 <ul>
     <li>Login: {{ $admin->login }}</li>
     <li>Mot de passe: {{ $password }}</li>
     </ul>
-
+    </div>
+</div>
--- resources/views/emails/user_registration.blade.php
+++ resources/views/emails/user_registration.blade.php
@@ -1,9 +1,14 @@
 <!-- resources/views/emails/user_registration.blade.php -->
-<p>Hello {{ $user->prenom }},</p>
-<p>Your user account has been successfully created. Here are your login details:</p>
-<ul>
-    <li>Login: {{ $user->login }}</li>
-    <li>Mot de passe: {{ $password }}</li>
-</ul>
-
-
+<div style="border: 1px solid #ccc; padding: 10px; width: 300px;">
+    <div style="background-color: #004085; color: #fff; padding: 10px;">
+        <h2 style="color: white;">User Registration</h2>
+    </div>
+    <div style="padding: 10px;">
+        <p>Hello {{ $user->prenom }},</p>
+        <p>Your user account has been successfully created. Here are your login details:</p>
+        <ul>
+            <li>Login: {{ $user->login }}</li>
+            <li>Mot de passe: {{ $password }}</li>
+        </ul>
+    </div>
+</div>
